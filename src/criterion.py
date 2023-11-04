8ca72289b2c95b625cbdd237cbd8366156372682
mlpotter <mlpotter96@gmail.com>
2023-11-04 11:48:29 -0400

    add model.py and criterion.py file in pytorch

---
+++ src/criterion.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+def interval_censored(rate,t_start,t_end):
+    pass
+
+def right_censored(rate,t_exact,t_start):
+    pass
+
+def left_censored(rate,t_exact,t_end):
+    pass
+
+
---
+++ src/models.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Exponential_Model(nn.Module):
+    def __init__(self,input_dim,hidden_layers=[10],output_dim=1):
+        super().__init__()
+        self.layers = [input_dim] + hidden_layers + [output_dim]
+        self.linears = nn.ModuleList([nn.Linear(self.layers[i],self.layers[i+1]) for i in range(len(self.layers)-1)])
+        self.num_layers = len(self.layers)
+
+    def rate(self,x):
+        for i,l in enumerate(self.linears):
+            x = l(x)
+
+            if i == (self.num_layers-2):
+                break
+
+            x = F.leaky_relu(x)
+
+        return torch.exp(x)
+
+    def forward(self,x):
+
+        rate = self.rate(x)
+
+        return rate
+
+
+def main():
+    input_dim = 5
+    hidden_layers = [10,10]
+    output_dim = 1
+
+    batch_size = 10
+
+    model = Exponential_Model(input_dim=input_dim,hidden_layers=hidden_layers,output_dim=output_dim)
+
+    x = torch.randn(batch_size,input_dim)
+    print(model)
+    print(model.layers)
+    rate = model(x)
+    print(rate)
+
+
+if __name__ == "__main__":
+    main()
+
